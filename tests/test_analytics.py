ed9fda02428e2e7734f897b62c973cdf46e2a038
vinostroud <krstrouse@gmail.com>
2024-06-22 11:31:45 -0400

    improved test code and added fixtures

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -28,46 +28,54 @@ def test_validate_columns():
             'defteam': Column(str),
         }
     )
-    schema.validate(df)
+    try:
+        schema.validate(df)
+    except pa.errors.SchemaError as e:
+        pytest.fail(f"Schema validation failed: {e}")
     
 
-# Now that we've established my columns are valid, create sample data to mock the nfl.import_pbp_data function. 
-sample_data = pd.DataFrame({
-    'season_type': ['REG', 'REG', 'POST'],
-    'pass': [1, 0, 1],
-    'rush': [0, 1, 0],
-    'epa': [0.5, None, 0.2],
-    'posteam': ['TeamA', 'TeamB', 'TeamC'],
-    'defteam': ['TeamD', None, 'TeamE']
-})
 
-#Test 2:  for load_data(df_year: int)
-def test_load_data():
+
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame({
+        'season_type': ['REG', 'REG', 'POST'],
+        'pass': [1, 0, 1],
+        'rush': [0, 1, 0],
+        'epa': [0.5, None, 0.2],
+        'posteam': ['TeamA', 'TeamB', 'TeamC'],
+        'defteam': ['TeamD', None, 'TeamE']
+    })
+
+@pytest.fixture
+def mock_data():
+    return pd.DataFrame({
+        'down': [1, 1, 1, 1, 1],
+        'posteam': ['TeamA', 'TeamA', 'TeamB', 'TeamA', 'TeamB'],
+        'epa': [0.5, 0.7, 0.3, 0.6, 0.4]
+    })
+
+@pytest.fixture
+def expected_output():
+    return pd.DataFrame({
+        'Team': ['TeamA', 'TeamB'],
+        'First Down EPA': [0.6, 0.35],
+        'Rank': [1.0, 2.0]
+    })
+
+def test_load_data(sample_data):
     '''Test that load_data returns a DataFrame'''
     df_year = 2015
     with patch('nfl_data_py.import_pbp_data', return_value=sample_data) as mock_import:
         test_df = load_data(df_year)
-        assert isinstance(test_df, DataFrame), "The output is not a DataFrame"
+        assert isinstance(test_df, pd.DataFrame), "The output is not a DataFrame"
         mock_import.assert_called_once_with([df_year])
 
-
-#Test 3 : For get_mean_epa_down1 - test uses mock data to confirm order is correct
-
-mock_data = pd.DataFrame({
-    'down': [1, 1, 1, 1, 1],
-    'posteam': ['TeamA', 'TeamA', 'TeamB', 'TeamA', 'TeamB'],
-    'epa': [0.5, 0.7, 0.3, 0.6, 0.4]
-})
-
-expected_output = pd.DataFrame({
-    'Team': ['TeamA', 'TeamB'],
-    'First Down EPA': [0.6, 0.35],
-    'Rank': [1.0, 2.0]
-})
-
-def test_get_mean_epa_down1():
+def test_get_mean_epa_down1(mock_data, expected_output):
     result = get_mean_epa_down1(mock_data)
     pd.testing.assert_frame_equal(result, expected_output)
+
+
 
 '''
 Placeholder for Test 4 - getmeanepadown1and2
